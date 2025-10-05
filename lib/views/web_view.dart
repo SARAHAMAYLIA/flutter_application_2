aec2f981b563eebedceafd1826d1075f0e0a3918
sarahamaylia <sarahamaylia10@gmail.com>
2025-10-05 10:47:02 +0700

    menambahkan webview.dart

--- lib/views/web_view.dart
+++ lib/views/web_view.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewPage extends StatelessWidget {
+class WebViewPage extends StatefulWidget {
   const WebViewPage({super.key});
 
   @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse("https://kampusmu.ac.id")); // ganti URL
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text("Kampus Website"),
+        title: const Text("Kampus Website"),
       ),
-      body: WebView(
-        initialUrl: "https://kampusmu.ac.id", // ganti dengan URL kampusmu
-        javascriptMode: JavascriptMode.unrestricted,
-      ),
+      body: WebViewWidget(controller: _controller),
     );
   }
 }
